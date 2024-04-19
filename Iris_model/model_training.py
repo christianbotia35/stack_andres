0f902e4f4e6075c47f1385c2ca128d9d115fdd12
Andres Ortega <afortegast@gmail.com>
2024-04-19 21:08:49 +0200

    test

--- Iris_model/model_training.py
+++ Iris_model/model_training.py
@@ -1,34 +1,27 @@
 import mlflow
 import mlflow.sklearn
-import pickle
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
-# Load Boston housing dataset
-boston = load_boston()
-X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, test_size=0.2, random_state=42)
+# Load Iris dataset
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
 
-# Train a simple linear regression model
-model = LinearRegression()
+# Train a logistic regression model
+model = LogisticRegression()
 model.fit(X_train, y_train)
 
 # Evaluate the model
 y_pred = model.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error: {mse}")
-
-# Save the model as a pickle file
-model_path = "linear_regression_model.pkl"
-with open(model_path, "wb") as f:
-    pickle.dump(model, f)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
 
 mlflow_url = "http://mlflow-server-service.mlflow:5000"
-# mlflow_url = "http://localhost:5000"
-mlflow.set_tracking_uri(mlflow_url)
-mlflow.set_experiment('boston-model')
 
+mlflow.set_tracking_uri(mlflow_url)
+mlflow.set_experiment('iris-model')
 # Log the model and parameters in MLflow
 with mlflow.start_run():
     # Log parameters
@@ -36,7 +29,7 @@ with mlflow.start_run():
     mlflow.log_param("random_state", 42)
 
     # Log metrics
-    mlflow.log_metric("mean_squared_error", mse)
+    mlflow.log_metric("accuracy", accuracy)
 
-    # Log model as an artifact
-    # mlflow.log_artifact(model_path, "model")
+    # Log model
+    # mlflow.sklearn.log_model(model, "logistic_regression_model")
